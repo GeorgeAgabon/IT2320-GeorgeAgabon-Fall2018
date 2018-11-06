16d44b4fe834230e359ee64e79c7b79e1e56ad0f
George Agabon <s00004951@acad.tri-c.edu>
2018-11-05 22:44:40 -0500

    Lab 8 jquery traversal

--- Week 8/home.js
+++ Week 8/home.js
@@ -27,50 +27,77 @@ $(function(){
     $("#btn5").on("click",function(){
         $("#wrapperSection1").parents("#content").css({"color": "orange"});
     });
-
-     
         
     // Additional siblings method
     //next 
     $("#next").on("click", function(){
-        $("#wrapperSection1").next().css({"color": "orange"});
+        $("#wrapperSection1").next().css({"background-color": "blue"});
     });
+
     //nextAll
     $("#nextAll").on("click", function(){
-        $("#wrapperSection1").nextAll().css({"color": "green"});
+        $("#wrapperSection1").nextAll().css({"color": "green", "background-color": "blue"});
     });
+
     //nextUntil
     $("#nextUntil").on("click", function(){
         $("#wrapperSection1").nextUntil("#wrapperSection3").css({"background-color": "olive",
-    "boder": "1px solid black"});
+    "border": "1px solid black"});
     });
+    
     //closest
-    
-    
+    $("#closest").on("click", function(){
+        $("#wrapperSection1").closest("#container").css({"color" : "green",
+                            "border" : "2px solid red"});
+    });
     
     //prev
-    //prevAll 
+    $("#prev").on("click", function(){
+        $("#wrapperSection3").prev().css({"background-color": "orange"});
+    });
+
+    //prevAll
+    $("#prevAll").on("click", function(){
+        $("#wrapperSection3").prevAll().css({"background-color": "pink"});
+    });
+
     //prevUntil
-    
+    $("#prevUntil").on("click", function(){
+        $("#wrapperSection3").prevUntil("#container").css({"color": "blue", "border": "2px solid black" });
+    });
     
     // use "find" to do this
     //first
+    $("#first").on("click", function(){
+        $(".wrapper").first().css({"background-color": "yellow"});
+    });
     //last
+    $("#last").on("click", function(){
+        $(".wrapper").last().css({"background-color": "purple", "color": "white"});
+    });
+
     //filter 
+    $("#filter").on("click", function(){
+        $("p").filter(".para").css({"background-color": "yellow", "color": "blue"});
+    });
+
     //not
-    
-    
+    $("#notincluding").on("click", function(){
+        $("p").not(".para").css("background-color", "blue");
+    });
     
     //slice
+    $("#slice").on("click", function(){
+        $("p").slice(3).css({"background-color": "pink", "color": "green"});
+    });
+
+});
+
     
     
     
-    })
-    
-    
-    
-    function reset()
-    
+    //function reset()
+     
     {
     
         $("#wrapperSection").parent().css("color","black");
@@ -81,4 +108,5 @@ $(function(){
     
         $("#wrapperSection").find("*").css("color","black");
     
-    }
+    };
+
